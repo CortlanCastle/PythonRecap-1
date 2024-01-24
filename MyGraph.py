efd3afcb687bad6e24ac5d38d666238c033ad181
Cortlan <Cortlan_Castle1@baylor.edu>
2024-01-24 13:28:56 -0600

    updated gitiginore and changed print statement

--- MyGraph.py
+++ MyGraph.py
@@ -6,4 +6,8 @@ plt.plot(x, np.sin(x))
 
 plt.show()
 
-print("Hi there!")
+print("Hello World!")
+# You have to install a library to make this work. # Create a virtual environment every new project and copy libraries into it
+# step 1 - Create a virtual environment- (do in terminal down below) - PC's use "py" MAC use "python3"--- EX: py -3 -m venv myvenv (myvenv is the name)
+# step 2 - Activate your venv. .\myvenv\Scripts\activate
+# Step 3 - Install 3rd party library/modeule - pip3 install matplotlib (matplotlib is name of library)git confi
